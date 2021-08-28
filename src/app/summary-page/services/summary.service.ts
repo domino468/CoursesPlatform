624c913cc485e7f238b33d5024e8959706f24ea5
LukaszGryziewicz <lukaszgryziewicz@gmail.com>
2021-08-28 11:32:14 +0200

    you can now see the email you provided and lectures you picked on summary page

--- src/app/summary-page/services/summary.service.ts
+++ src/app/summary-page/services/summary.service.ts
@@ -1,7 +1,27 @@
 import {Injectable} from '@angular/core';
 
+
 @Injectable()
 export class SummaryService {
+  private _email: string = '';
+
+  get email(): string {
+    return this._email;
+  }
+
+  set email(value: string) {
+    this._email = value;
+  }
+
+  private _lectures: string[] = [];
+
+  get lectures(): string[] {
+    return this._lectures;
+  }
+
+  set lectures(value: string[]) {
+    this._lectures = value;
+  }
 
   constructor() {
   }
--- src/app/summary-page/summary-page.component.ts
+++ src/app/summary-page/summary-page.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SummaryService} from './services/summary.service';
 
 @Component({
   selector: 'app-summary-page',
   templateUrl: './summary-page.component.html',
   styleUrls: ['./summary-page.component.css']
 })
-export class SummaryPageComponent  {
+export class SummaryPageComponent implements OnInit {
+  lecturesToDisplay: String[] = [];
+  emailToDisplay: String = "";
+
+  constructor(private summaryService: SummaryService) {
+  }
+
+  ngOnInit(): void {
+    this.lecturesToDisplay = this.summaryService.lectures;
+    this.emailToDisplay = this.summaryService.email;
+  }
 
 
-
- 
-  
 }
