7bd24be9e8940f48f7686ab51d7ee915377dd967
Adam Dominik <dominik.adam4538@gmail.com>
2021-09-18 09:46:16 +0200

    improved summary page commit

--- src/app/summary-page/summary-page.component.ts
+++ src/app/summary-page/summary-page.component.ts
@@ -4,7 +4,7 @@ import {SummaryService} from './services/summary.service';
 @Component({
   selector: 'app-summary-page',
   templateUrl: './summary-page.component.html',
-  styleUrls: ['./summary-page.component.css']
+  styleUrls: ['./summary-page.component.scss']
 })
 export class SummaryPageComponent implements OnInit {
   lecturesToDisplay: String[] = [];
